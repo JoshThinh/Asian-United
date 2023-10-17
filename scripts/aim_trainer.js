75de91dfedda9c26f03c74aee68d4846ffd38a30
JoshThinh <joshua.thinh@gmail.com>
2023-10-16 19:04:38 -0700

    deleted unnecesary things

--- scripts/aim_trainer.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Part
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  readline.question('Enter your name: ', (name) => {
-    console.log(`Welcome, ${name}!`);
-    readline.close();
-  });
