17ce93f7bd86e48e7349955b7149fa40bdd11209
JoshThinh <joshua.thinh@gmail.com>
2023-10-23 12:41:05 -0700

    added js buttons

---
+++ button.js
@@ -0,0 +1,11 @@
+function game() {
+    window.location.href = 'game.html'; 
+}
+
+function home() {
+    window.location.href = 'index.html'; 
+}
+
+function about() {
+    window.location.href = 'about.html'; 
+}
