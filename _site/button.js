3fede9c4747c04c2cd781ec7f620c5372311b607
7mwang <matthewxpwang@gmail.com>
2023-10-30 23:00:21 -0700

    little syntax

--- _site/button.js
+++ _site/button.js
@@ -9,10 +9,3 @@ function home() {
 function about() {
     window.location.href = 'about.html'; 
 }
-document.getElementById("launchButton").addEventListener("click", function() {
-    // Specify the path to your game file within your folder
-    var gameFilePath = "games/Cookie_Clicker/CookieClicker.html"; // Change this to the actual path
-    
-    // Open the game file in a new window or tab
-    window.open(gameFilePath, "_blank");
-});
